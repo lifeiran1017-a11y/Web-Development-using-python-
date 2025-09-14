041b0bd29b9457e71b4815520834c1c32b46709f
lifeiran1017-a11y <lifeiran1017@gmail.com>
2025-09-14 11:41:05 +0800

    Add files via upload

---
+++ MyFlask/app.py
@@ -0,0 +1,13 @@
+from flask import Flask,render_template
+app = Flask(__name__)
+@app.route('/')
+def greet():
+    return render_template("greet.html", name="Annie")
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
